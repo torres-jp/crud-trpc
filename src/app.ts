cf0fc9273b9f80e5037dd1566cdda51d47e06b62
pnauj <torres.jpw@gmail.com>
2023-10-26 21:40:32 -0300

    se realizo la configuracion del be

---
+++ src/app.ts
@@ -0,0 +1,25 @@
+import express from "express";
+import morgan from "morgan";
+import * as trpcExpress from "@trpc/server/adapters/express";
+import { router, createContext } from "./trpc";
+import { notesRouter } from "./routes/notes";
+
+const app = express();
+
+const appRouter = router({
+  note: notesRouter,
+});
+
+app.use(morgan("dev"));
+
+app.use(
+  "/trpc",
+  trpcExpress.createExpressMiddleware({
+    router: appRouter,
+    createContext,
+  })
+);
+
+export default app;
+
+//00:25:00
